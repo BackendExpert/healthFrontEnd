0e345aaafd58c4ada78e7ffc1e884e8382b0c3cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-10 16:15:12 +0530

    Update Testimonials.jsx

--- src/components/HomePage/Testimonials.jsx
+++ src/components/HomePage/Testimonials.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div>
+    <div data-aos="zoom-in">
         <h1 className="font-semibold text-2xl text-center">What Our Patients Say</h1>
+
+        <div  className="text-gray-500 text-center my-4">
+            "Don’t just take our word for it—hear from our patients about their experiences at [Healthcare Center Name]. We’re proud of the positive impact we’ve had on our patients’ lives and are committed to providing exceptional care."
+        </div>
     </div>
   )
 }
