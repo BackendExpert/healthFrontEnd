1df3385fc8befb44cf56c8678054518feca6082d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-11 16:04:01 +0530

    Update

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const checkAuth = async (e) => {
+            try{
+                const token = localStorage.getItem('token')
+
+                if(!token){
+                    navigate('/PatientPortal')
+                    window.localStorage.reload()
+                }
+
+                const response = await axios.get(import.meta.env.VITE_APP_API + '/dashboard', {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    },
+                });
+            }
+            catch(err) {
+                console.log(err)
+            }
+        }
+        checkAuth()
+    }, [navigate])
   return (
     <div>Dashboard</div>
   )
