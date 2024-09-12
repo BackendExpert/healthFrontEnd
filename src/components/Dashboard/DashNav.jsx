7c80ab755b8d78d8e3d0d7a2d256ae9618fe7454
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-12 14:58:48 +0530

    Update

--- src/components/Dashboard/DashNav.jsx
+++ src/components/Dashboard/DashNav.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import UserImg from '../../assets/User.png'
 import axios from 'axios'
+import  secureLocalStorage  from  "react-secure-storage"
+import { useNavigate } from 'react-router-dom'
 
 const DashNav = () => {
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
     const [DashData, SetDashData] = useState([])
 
     useEffect(() => {
-      axios.get(import.meta.env.VITE_APP_API + '/Dashboard/DashData')
+      axios.get(import.meta.env.VITE_APP_API + `/Dashboard/DashData/${EmailUser}`)
       .then(res => SetDashData(res.data.Result))
       .catch(err => console.log(err))
     }, [])
@@ -19,8 +25,8 @@ const DashNav = () => {
             </div>
             <div className="flex">
               <div className="">
-                <h1 className="text-sm pr-2">JehanKandy</h1>
-                <h1 className="text-sm pr-2 uppercase text-right text-gray-500 font-bold">Admin</h1>
+                <h1 className="text-sm pr-2">{DashData.username}</h1>
+                <h1 className="text-sm pr-2 uppercase text-right text-gray-500 font-bold">{DashData.role}</h1>
               </div>
               <img src={UserImg} alt="" className='h-10 w-auto rounded-full'/>
             </div>
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import DashSide from './DashSide';
 import DashNav from './DashNav';
 import secureLocalStorage from "react-secure-storage";
+import DashFooter from './DashFooter';
+
 
 const Dashboard = () => {
   return (
@@ -17,6 +19,7 @@ const Dashboard = () => {
           <div className="py-4 px-2 md:mr-4  md:mx-0 ml-4 mr-4">
             <Outlet />
           </div>
+          <DashFooter />
         </div>
       </div>
     </div>
