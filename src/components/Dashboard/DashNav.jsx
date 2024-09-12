b7c59f2cedae4a23749b560bcc91c4c539935c14
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-12 14:06:20 +0530

    Update

--- src/components/Dashboard/DashNav.jsx
+++ src/components/Dashboard/DashNav.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserImg from '../../assets/User.png'
+import axios from 'axios'
 
 const DashNav = () => {
+    const [DashData, SetDashData] = useState([])
+
+    useEffect(() => {
+      axios.get(import.meta.env.VITE_APP_API + '/Dashboard/DashData')
+      .then(res => SetDashData(res.data.Result))
+      .catch(err => console.log(err))
+    }, [])
+    
   return (
     <div className='py-4 mr-2 px-4 '>
         <div className="flex justify-between">
