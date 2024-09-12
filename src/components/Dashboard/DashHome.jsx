35a465259cc190748a86d7ff979734107305a43b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-12 15:07:34 +0530

    Update

--- src/components/Dashboard/DashHome.jsx
+++ src/components/Dashboard/DashHome.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import  secureLocalStorage  from  "react-secure-storage"
+import { useNavigate } from 'react-router-dom'
 import DoctorImg from '../../assets/Doctor.png'
+import PatentImg from '../../assets/PatientImg.png'
 
 const DashHome = () => {
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    const [DashData, SetDashData] = useState([])
+
+    useEffect(() => {
+      axios.get(import.meta.env.VITE_APP_API + `/Dashboard/DashData/${EmailUser}`)
+      .then(res => SetDashData(res.data.Result))
+      .catch(err => console.log(err))
+    }, [])
   return (
     <div>
         <h1 className="font-semibold uppercase text-xl">Dashboard</h1>
@@ -9,11 +24,35 @@ const DashHome = () => {
         <div className="bg-white p-4 my-2 rounded-md shadow-md">
             <div className="md:flex">
                 <div className="w-full">
-                    <p className="text-xl">Welcome Doctor, <span className='font-semibold'>Kamal Perera</span></p>
+                    <p className="text-xl">Welcome {DashData.role}, <span className='font-semibold'>{DashData.username}</span></p>
                     <p className="text-gray-500 mt-2">Have a nice Day</p>
                 </div>
                 <div className="w-full">
-                    <center><img src={DoctorImg} alt="" className='h-40 w-auto'/></center>
+                    {
+                        (() => {
+                            if(DashData.role === "Doctor"){
+                                return (
+                                    <center><img src={DoctorImg} alt="" className='h-40 w-auto'/></center>
+                                )
+                            }
+                            else if(DashData.role === "Patient"){
+                                return (
+                                    <center><img src={PatentImg} alt="" className='h-40 w-auto'/></center>
+                                )
+                            }
+                            else if(DashData.role === "Admin"){
+                              return (
+                                  <center><img src={DoctorImg} alt="" className='h-40 w-auto'/></center>
+                              )
+                            }
+                            else if(DashData.role === "Nurse"){
+                              return (
+                                  <center><img src={DoctorImg} alt="" className='h-40 w-auto'/></center>
+                              )
+                            }
+                        })()
+                    }
+                    
                 </div>
             </div>
         </div>
