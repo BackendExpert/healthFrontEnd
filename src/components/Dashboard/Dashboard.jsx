e3eb7397d081fc4f8dee4c31bba0d23bb0375ce3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-16 22:14:12 +0530

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,24 +6,36 @@ import DashNav from './DashNav';
 import secureLocalStorage from "react-secure-storage";
 import DashFooter from './DashFooter';
 
-
 const Dashboard = () => {
-  return (
-    <div className="w-full min-h-screen bg-blue-100/50 ">
-      <div className="md:flex">
-        <div className="w-1/5 fixed top-0 left-0 h-full">
-          <DashSide />
-        </div>
-        <div className="md:w-full md:ml-[20%]"> 
-          <DashNav />
-          <div className="py-4 px-2 md:mr-4  md:mx-0 ml-4 mr-4">
-            <Outlet />
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(EmailUser !== null || RoleUser !== null){
+      return (
+        <div className="w-full min-h-screen bg-blue-100/50 ">
+          <div className="md:flex">
+            <div className="w-1/5 fixed top-0 left-0 h-full">
+              <DashSide />
+            </div>
+            <div className="md:w-full md:ml-[20%]"> 
+              <DashNav />
+              <div className="py-4 px-2 md:mr-4  md:mx-0 ml-4 mr-4">
+                <Outlet />
+              </div>
+              <DashFooter />
+            </div>
           </div>
-          <DashFooter />
         </div>
-      </div>
-    </div>
-  );
+      );
+    }
+    else{
+      useEffect(() => {
+        localStorage.clear()
+        navigate('/PatientPortal')
+      }, [])
+    }
+
 };
 
 export default Dashboard;
