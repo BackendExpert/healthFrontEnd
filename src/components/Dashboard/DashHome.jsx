a6dd3e1e4c1f353460dd6eff12dd33732f04fabe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-04 08:34:17 +0530

    Update DashHome.jsx

--- src/components/Dashboard/DashHome.jsx
+++ src/components/Dashboard/DashHome.jsx
@@ -20,47 +20,56 @@ const DashHome = () => {
       .then(res => SetDashData(res.data.Result))
       .catch(err => console.log(err))
     }, [])
-  return (
-    <div>
-        <h1 className="font-semibold uppercase text-xl">Dashboard</h1>
 
-        <div className="bg-white p-4 my-2 rounded-md shadow-md">
-            <div className="md:flex">
-                <div className="w-full">
-                    <p className="text-xl">Welcome {DashData.role}, <span className='font-semibold'>{DashData.username}</span></p>
-                    <p className="text-gray-500 mt-2">Have a nice Day</p>
-                </div>
-                <div className="w-full">
-                    {
-                        (() => {
-                            if(DashData.role === "Doctor"){
-                                return (
-                                    <center><img src={DoctorImg} alt="" className='h-40 w-auto'/></center>
-                                )
+    if(EmailUser !== null || RoleUser !== null){
+        return (
+            <div>
+                <h1 className="font-semibold uppercase text-xl">Dashboard</h1>
+        
+                <div className="bg-white p-4 my-2 rounded-md shadow-md">
+                    <div className="md:flex">
+                        <div className="w-full">
+                            <p className="text-xl">Welcome {DashData.role}, <span className='font-semibold'>{DashData.username}</span></p>
+                            <p className="text-gray-500 mt-2">Have a nice Day</p>
+                        </div>
+                        <div className="w-full">
+                            {
+                                (() => {
+                                    if(DashData.role === "Doctor"){
+                                        return (
+                                            <center><img src={DoctorImg} alt="" className='h-40 w-auto'/></center>
+                                        )
+                                    }
+                                    else if(DashData.role === "Patient"){
+                                        return (
+                                            <center><img src={PatentImg} alt="" className='h-40 w-auto'/></center>
+                                        )
+                                    }
+                                    else if(DashData.role === "SuperAdmin"){
+                                      return (
+                                        <center><img src={SuperAdminImg} alt="" className='h-40 w-auto'/></center>
+                                      )
+                                    }
+                                    else if(DashData.role === "Nurse"){
+                                      return (
+                                          <center><img src={NurseImg} alt="" className='h-40 w-auto'/></center>
+                                      )
+                                    }
+                                })()
                             }
-                            else if(DashData.role === "Patient"){
-                                return (
-                                    <center><img src={PatentImg} alt="" className='h-40 w-auto'/></center>
-                                )
-                            }
-                            else if(DashData.role === "SuperAdmin"){
-                              return (
-                                <center><img src={SuperAdminImg} alt="" className='h-40 w-auto'/></center>
-                              )
-                            }
-                            else if(DashData.role === "Nurse"){
-                              return (
-                                  <center><img src={NurseImg} alt="" className='h-40 w-auto'/></center>
-                              )
-                            }
-                        })()
-                    }
-                    
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-  )
+          )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/PatientPortal')
+        }, [])
+    }
 }
 
 export default DashHome
