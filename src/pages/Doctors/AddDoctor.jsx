8e52e96251417ddc202c00e5f81c1e4b93401b57
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-10 13:54:53 +0530

    Update

--- src/pages/Doctors/AddDoctor.jsx
+++ src/pages/Doctors/AddDoctor.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddDoctor = () => {
+    const [DoctorData, SetDoctorData] = useState({
+        doc_name: '',
+        doc_email: '',
+        doc_address: '',
+        doc_nic: '',
+        doc_hospital: '',
+        is_work: '',
+        doc_area: '',
+    })
+
+    const handleChange = (e) => {
+       const { name, value } = e.target;
+       SetDoctorData((prevFormData) => ({
+         ...prevFormData,
+         [name]: value,
+       }));
+    };
+
+    const headleDoctorCreate = (e) => {
+        e.preventDefault()
+
+        try{
+            console.log(DoctorData)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
   return (
     <div>AddDoctor</div>
   )
